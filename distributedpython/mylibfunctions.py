9ff9f33f59f2f078fe9dcc42c5f9e40fc0c64fd9
George Shao <georgeshao123@gmail.com>
2020-08-14 22:10:21 -0400

    Finally fixed stdout error after 3-4hrs of debugging!

--- distributedpython/mylibfunctions.py
+++ distributedpython/mylibfunctions.py
@@ -1,30 +1,33 @@
 import subprocess
 import os
-
 def run(payload, function):
-	placeholder_js_file = open("placeholder_events.js", "r")
-	example_js_file = open("example_events.js", "r")
-	main_js_file = open("events.js", "w")
+
+	placeholder_js_file = open("./distributedpython/placeholder_events.js", "r")
+	example_js_file = open("./distributedpython/example_events.js", "r")
+	main_js_file = open("./distributedpython/events.js", "w")
 	placeholder_js_text = placeholder_js_file.read()
 	placeholder_js_text = placeholder_js_text.replace("[PAYLOAD]", payload)
 	placeholder_js_text = placeholder_js_text.replace("{FUNCTION}", function)
 	main_js_file.write(placeholder_js_text)
+	placeholder_js_file.close()
+	example_js_file.close()
+	main_js_file.close()
 
 	try:
-		process = subprocess.Popen(['node', 'events.js', '--scheduler=https://demo-scheduler.distributed.computer/'], 
-								stdout=subprocess.PIPE,
-								stderr=subprocess.PIPE,
-								universal_newlines=True)
+		process = subprocess.Popen(['node', './distributedpython/events.js', '--scheduler=https://demo-scheduler.distributed.computer/'], 
+									stdout=subprocess.PIPE,
+									stderr=subprocess.PIPE,
+									universal_newlines=True)
 	except Exception as e:
 		print("Exception:", e)
 
 	while True:
 		output = process.stdout.readline()
-		print('b', output.strip())
+		print(output.strip())
 		# Do something else
 		return_code = process.poll()
 		if return_code is not None:
 			# Process has finished, read rest of the output
 			for output in process.stdout.readlines():
-				print('a', output.strip())
+				print(output.strip())
 			return return_code
