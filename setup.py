7bf26ba690781523e76436fdc95600a11fd6444a
George Shao <georgeshao123@gmail.com>
2020-08-15 01:28:18 -0400

    Updated setuptools config

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 setup(
     name="distributedpython",
     packages=find_packages(include=['distributedpython']),
-    version="0.1.0",
-    description="Run Python code on the Distributed Compute Protocol",
+    version="1.0.0",
+    description="Python library to run Python code on the Distributed Compute Protocol",
     author="George Shao",
     author_email="georgeshao123@gmail.com",
     license='gpl-3.0',
