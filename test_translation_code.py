cce1d71c3ef385df147d5d7cc5291663589c16c3
George Shao <georgeshao123@gmail.com>
2020-08-15 01:03:20 -0400

    Removed old testing files

--- test_translation_code.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-try:
-	process = subprocess.Popen(['python', '-m', 'metapensiero.pj', 'test_translation.py'], 
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                universal_newlines=True)
-except Exception as e:
-    print("Exception:", e)
--- transpile_temp.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=transpile_temp.js.map
